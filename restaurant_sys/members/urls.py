78e68ac43a7b9ef3035bcf61c75e97994c826111
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-10-24 22:39:27 +0700

    Update urls.py

--- restaurant_sys/members/urls.py
+++ restaurant_sys/members/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('testing/', views.testing, name='testing'),
-    
+    path('search/', views.search_orders, name='search_orders'),
+
 ]

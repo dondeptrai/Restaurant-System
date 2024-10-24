b3384cdb4258464ef02788af09bf2b1b2ffd3ec2
mydayoanhh <163551598+mydayoanhh@users.noreply.github.com>
2024-10-24 21:52:26 +0700

    Update urls.py

--- restaurant_sys/members/urls.py
+++ restaurant_sys/members/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('testing/', views.testing, name='testing'),
+    path('register/', views.register, name='register'),
+   path('login/', views.login, name='login'),
     
 ]
